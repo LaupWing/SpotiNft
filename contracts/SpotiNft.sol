fe067cd44cc10fad92ac71d0ac3ae778279cbc3c
LaupWing <loc_strict@hotmail.com>
2023-02-07 15:24:43 +0100

    udpate

--- contracts/SpotiNft.sol
+++ contracts/SpotiNft.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.9;
 
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract SpotiNftMarketplace {
    address payable public owner;
@@ -13,4 +15,14 @@ contract SpotiNftMarketplace {
 }
 
 
-contract 
+contract SpotiNftAlbum is ERC721URIStorage {
+   constructor (
+      string memory albumName, 
+      string memory albumSymbol
+   ) ERC721(
+      albumName,
+      albumSymbol
+   ) {
+      
+   }
+}
