c17f36a383b4c526e5296f013ca551765e60bcfd
LaupWing <loc_strict@hotmail.com>
2023-02-10 15:40:30 +0100

    base

--- contracts/SpotiNft.sol
+++ contracts/SpotiNft.sol
@@ -6,13 +6,26 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract SpotiNftMarketplace is ERC721URIStorage {
+contract SpotiNftMarketplace {
    address payable public owner;
+
+   constructor() {
+      owner = payable(msg.sender);
+   }
+}
+
+
+contract SpotiAlbum  is ERC721URIStorage{
+   address payable public owner;
+
    using Counters for Counters.Counter;
 
-   constructor() ERC721(
-      "SpotiNft",
-      "SP"
+   Counters.Counter private tokenIds;
+   Counters.Counter private itemsSold;
+
+   constructor( string memory _name, string memory _symbol) ERC721(
+      _name,
+      _symbol
    ) {
       owner = payable(msg.sender);
    }
