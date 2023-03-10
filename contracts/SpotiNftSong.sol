cb214f11db54bd40d2d5028045a74ba7cebe9c2f
LaupWing <loc_strict@hotmail.com>
2023-03-10 10:20:39 +0100

    base spoti song

--- contracts/SpotiNftSong.sol
+++ contracts/SpotiNftSong.sol
@@ -4,13 +4,17 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
+error SpotiNftSong__NotEoughEthSend();
+
 contract SpotiNftSong is ERC721{
-   Counters.Counter private songBoughtId;
+   using Counters for Counters.Counter;
+   Counters.Counter private tokenId;
 
    string private uri;
    string private name;
    uint256 private mintFee;
-   address album;
+   address[] private owners;
+   address private album;
 
    constructor(
       string memory _uri,
@@ -24,10 +28,32 @@ contract SpotiNftSong is ERC721{
    }
 
    function mintSong() public payable {
-
+      if(msg.value < mintFee){
+         revert SpotiNftSong__NotEoughEthSend();
+      }
+      tokenId.increment();
+      uint256 newTokenId = tokenId.current();
+      _safeMint(msg.sender, newTokenId);
+      owners.push(msg.sender);
    }
 
-   function getTokenUri() public view returns(string memory){
+   function getUri() public view returns(string memory){
       return uri;
+   }
+
+   function getOwners() public view returns(address[] memory){
+      return owners;
+   }
+
+   function getAlbum() public view returns(address){
+      return album;
+   }
+
+   function getName() public view returns(string memory){
+      return name;
+   }
+
+   function getMintFee() public view returns(uint256){
+      return mintFee;
    }
 }
