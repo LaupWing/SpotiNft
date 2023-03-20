50e8a294c5eace2a0dc46c0f1aada4c4bb70b94e
LaupWing <loc_strict@hotmail.com>
2023-03-20 13:25:41 +0100

    update

--- contracts/SpotiNftMarketplace.sol
+++ contracts/SpotiNftMarketplace.sol
@@ -137,5 +137,7 @@ contract SpotiNftMarketplace is ERC721URIStorage {
       emit AlbumCreated(created_album_address, _name);
    }
 
-   function getAlbums() public view returns()
+   function getAlbums() public view returns(SpotiNftAlbum[] memory){
+      return albumsArray;
+   }
 }
